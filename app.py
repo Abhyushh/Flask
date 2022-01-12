fdafd4bedf902bcb020ad2b49023dc8dd2096914
aki09 <akshatbhatia05@gmail.com>
2022-01-12 14:49:42 +0530

    df

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response
 import cv2
 
 app = Flask(__name__)
@@ -6,13 +6,12 @@ camera = cv2.VideoCapture(0)
 
 
 def generate_frames():
-    return "hello dataattaat"
     while True:
 
         # read the camera frame
         success, frame = camera.read()
         if not success:
-            return "helloooo"
+            break
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
@@ -31,5 +30,4 @@ def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-if __name__ == "_main_":
-    app.run()
+app.run()
